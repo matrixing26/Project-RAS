59d0826a474d2248811918782cc8b3bc53a678ac
seetop <siumabon123@gmail.com>
2023-08-09 13:17:34 -0400

    update

--- datasets/solver.py
+++ datasets/solver.py
@@ -1,6 +1,6 @@
 import torch
 import numpy as np
-import time
+import math
 from typing import Callable, Tuple, Union, List, Any
 from torch import nn, Tensor
 from torch.nn import functional as F
@@ -77,13 +77,14 @@ class CVCSolver(nn.Module):
         self.upsample = upsample
         self.Mx = (Nx - 1) * upsample + 1
         self.Mt = (Nt - 1) * upsample + 1
-        self.register_buffer("xgrid", torch.linspace(xmin, xmax, Nx))
-        self.register_buffer("tgrid", torch.linspace(tmin, tmax, Nt))
+        xgrid = np.linspace(xmin, xmax, Nx)
+        tgrid = np.linspace(tmin, tmax, Nt)
+        self.grid = np.asarray(np.meshgrid(xgrid, tgrid, indexing="ij")).transpose([1,2,0])
         self.register_buffer("Xgrid", torch.linspace(xmin, xmax, self.Mx))
         self.register_buffer("Tgrid", torch.linspace(tmin, tmax, self.Mt))
         self.register_buffer("offdiag", torch.ones(self.Mx - 2).diag(-1))
-        self.dx = self.Xgrid[1] - self.Xgrid[0]
-        self.dt = self.Tgrid[1] - self.Tgrid[0]
+        self.dx = (xmax - xmin) / (self.Mx - 1)
+        self.dt = (xmax - xmin) / (self.Mt - 1)
         self.lam = self.dt / self.dx
         self.register_buffer("tk", torch.ones((self.Mx - 1,self.Mx - 1)).tril().unsqueeze(0))
     
@@ -118,10 +119,65 @@ class CVCSolver(nn.Module):
             buf = torch.einsum("bik,bk->bi", t_K, buf)
             viu[:, :, i + 1] = buf
         
-        x = self.xgrid.cpu().numpy()
-        t = self.tgrid.cpu().numpy()
-        return x, t, u[:, ::self.upsample, ::self.upsample].cpu().numpy()
+        return self.grid, u[:, ::self.upsample, ::self.upsample].cpu().numpy()
 
+class BurgersSolver(nn.Module):
+    def __init__(self, xmax: float = 1, tmax: float = 1, nu: float = 0.1, nx: int = 101, nt: int = 101, upsample: tuple[int, int] = (1, 5), umax: float = 5):
+        """
+        The settings for the Burgers equation solver.
+
+        Args:
+            xmax (float, optional): the length of x-axis. Defaults to 1.
+            tmax (float, optional): the length of t-axis. Defaults to 1.
+            nu (float, optional): the viscosity of the fluid. Defaults to 0.1.
+            nx (int, optional): the number of x grid. Defaults to 101.
+            nt (int, optional): the number of t grid. Defaults to 101.
+            upsample (tuple[int, int], optional): (up_x, up_t), affecting the precision of the solution, must satisfy some conditons. Defaults to None.
+            `up_x` should be bigger than `xmax / (nx * nu)` because of the shapeness of the shock wave. `up_t` should fulfill the stability condition (CFL), which is `CFL = nx * umax * tmax / nt / xmax < 1`.
+            umax (float, optional): the maximum of the initial condition. Defaults to 5.
+        """
+        super().__init__()
+        self.xmax = xmax
+        self.tmax = tmax
+        self.nu = nu
+        if upsample is None:
+            #TODO: automatically choose upsample
+            # 0.1 -> (1, 100), 0.01 -> (1, 10), 0.001 -> (1 , 10)
+            upx = max(1, math.ceil((xmax / (nx -1)) / nu))
+            upt = max(1, math.ceil(10 / nu * upx * (nx - 1) / (nt -1)))
+            upsample = (upx, upt)
+        self.upsample = upsample
+        self.nx = nx
+        self.nt = nt
+        self.mx = (nx -1) * upsample[0] + 1
+        self.mt = (nt - 1) * upsample[1] + 1
+        xgrid = np.linspace(0, xmax, nx)
+        tgrid = np.linspace(0, tmax, nt)
+        self.grid = np.asarray(np.meshgrid(xgrid, tgrid, indexing="ij")).transpose([1,2,0])
+        self.register_buffer("X", torch.linspace(0, xmax, self.mx))
+        self.dx = xmax / (self.mx - 1)
+        self.dt = tmax / (self.mt - 1)
+        self.c1 = self.dt / (2 * self.dx)
+        self.c2 = self.nu * self.dt / self.dx ** 2
+        
+    @torch.no_grad()
+    def forward(self, u0):
+        u0 = torch.as_tensor(u0, device=self.X.device, dtype=self.X.dtype)
+        batch = u0.shape[0]
+        u0 = interp_nd(u0, self.X[:, None] * 2 - 1)
+        u = torch.empty(batch, self.mx - 1, self.mt, device=self.X.device, dtype=self.X.dtype)
+        u[...,0] = u0[..., :-1]
+        for n in range(0, self.mt - 1):
+            u0 = u[..., n]
+            u1 = u0.roll(1, dims = 1)
+            u2 = u0.roll(-1, dims = 1)
+            u[..., n + 1] = u0 - self.c1 * (u2 - u1) * u0 + self.c2 * (u1 + u2 - 2 * u0)
+            
+        u = torch.cat([u, u[:, (0,), :]], dim = 1)
+        u = u[:, ::self.upsample[0], ::self.upsample[1]].cpu().numpy()
+        
+        return self.grid, u
+    
 def diffusion_reaction_solver(v: np.ndarray, xmax: float = 1.0, tmax: float = 1.0, D: float = 0.01, k: float = 0.01, Nx: int = 101, Nt: int = 101, *args, **kwargs) -> Tuple[np.ndarray, np.ndarray]:
     """
     Solve diffusion reaction equation: `u_t = D * u_xx - v(x) * u_x + k * u` with zero boundary condition.
@@ -159,19 +215,41 @@ def advection_solver(v: np.ndarray, xmax: float = 1.0, tmax: float = 1.0, Nx: in
     solver = CVCSolver(xmin = 0, xmax = xmax, tmin = 0, tmax = tmax, Nx = Nx, Nt = Nt, *args, **kwargs)
     if cuda:
         solver = solver.cuda()
+    else:
+        solver = solver.cpu()
     
     if batchsize is not None:
         split_num = int(np.ceil(v.shape[0] / batchsize))
         vs = np.array_split(v, split_num)
         us  = []
         for v in vs:
-            x, t, u = solver(v)
+            xt, u = solver(v)
             us.append(u)
         u = np.concatenate(us, axis = 0)
     else:
-        x, t, u = solver(v)
+        xt, u = solver(v)
     
-    xt = np.asarray(np.meshgrid(x, t, indexing = "ij")).transpose([1,2,0]) # shape (2, 101, 101)
+    # xt: shape (2, 101, 101) u: shape (b, 101, 101)
+    return xt, u
+
+def burger_solver(v: np.ndarray, xmax: float = 1.0, tmax: float = 1.0, nu = 0.1, Nx = 101, Nt = 101, cuda = True, batchsize: int = 100, *args, **kwargs) -> Tuple[np.ndarray, np.ndarray]:
+    solver = BurgersSolver(xmax = xmax, tmax = tmax, nu= nu, nx = Nx, nt = Nt, *args, **kwargs)
+    if cuda:
+        solver = solver.cuda()
+    else:
+        solver = solver.cpu()
+    
+    if batchsize is not None:
+        split_num = int(np.ceil(v.shape[0] / batchsize))
+        vs = np.array_split(v, split_num)
+        us  = []
+        for v in vs:
+            xt, u = solver(v)
+            us.append(u)
+        u = np.concatenate(us, axis = 0)
+    else:
+        xt, u = solver(v)
+    
     return xt, u
 
 #TODO
